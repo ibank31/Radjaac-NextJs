a57dc6f4494f10acb7aad490fd6aefe23a17da06
ibank31 <iqbalteguh01@gmail.com>
2026-05-30 14:26:13 +0700

    fix: forward props from WhatsApp link

--- components/ui/WhatsappLink.jsx
+++ components/ui/WhatsappLink.jsx
@@ -10,6 +10,7 @@ export default function WhatsappLink({
   category = "",
   pageType = "",
   waLabel = "",
+  ...props
 }) {
   const href = buildWhatsAppUrl({
     intent,
@@ -23,6 +24,7 @@ export default function WhatsappLink({
 
   return (
     <a
+      {...props}
       href={href}
       className={className}
       target="_blank"
